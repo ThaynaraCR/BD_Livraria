e83d7288d8373be28089c5430001b03d98c0ebc5
Thaynara C Rodrigues <60945730+ThaynaraCR@users.noreply.github.com>
2020-06-12 19:07:53 -0300

    Criação de tabelas
    Tabelas funcionario, cliente, e venda.

--- livraria.sql
+++ livraria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12-Jun-2020 às 00:46
+-- Tempo de geração: 13-Jun-2020 às 00:06
 -- Versão do servidor: 10.4.11-MariaDB
 -- versão do PHP: 7.2.29
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `livraria`
 --
-CREATE DATABASE IF NOT EXISTS `livraria` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `livraria`;
 
 -- --------------------------------------------------------
 
@@ -29,7 +27,6 @@ USE `livraria`;
 -- Estrutura da tabela `autor`
 --
 
-DROP TABLE IF EXISTS `autor`;
 CREATE TABLE `autor` (
   `id_autor` int(8) NOT NULL,
   `nome` varchar(30) DEFAULT NULL
@@ -38,10 +35,23 @@ CREATE TABLE `autor` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(8) NOT NULL,
+  `nome` varchar(20) DEFAULT NULL,
+  `cpf` int(11) DEFAULT NULL,
+  `telefone` int(11) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `editora`
 --
 
-DROP TABLE IF EXISTS `editora`;
 CREATE TABLE `editora` (
   `id_editora` int(8) NOT NULL,
   `nome` varchar(20) DEFAULT NULL
@@ -50,10 +60,23 @@ CREATE TABLE `editora` (
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `funcionario`
+--
+
+CREATE TABLE `funcionario` (
+  `id_funcionario` int(8) NOT NULL,
+  `nome` varchar(20) DEFAULT NULL,
+  `cpf` int(11) DEFAULT NULL,
+  `telefone` int(11) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `generolivro`
 --
 
-DROP TABLE IF EXISTS `generolivro`;
 CREATE TABLE `generolivro` (
   `id_genero` int(8) NOT NULL,
   `tipo` varchar(20) DEFAULT NULL
@@ -65,7 +88,6 @@ CREATE TABLE `generolivro` (
 -- Estrutura da tabela `livro`
 --
 
-DROP TABLE IF EXISTS `livro`;
 CREATE TABLE `livro` (
   `id_livro` int(8) NOT NULL,
   `id_editora` int(8) DEFAULT NULL,
@@ -74,6 +96,20 @@ CREATE TABLE `livro` (
   `nome` varchar(20) DEFAULT NULL,
   `paginas` int(8) DEFAULT NULL,
   `preço` float(3,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `venda`
+--
+
+CREATE TABLE `venda` (
+  `id_venda` int(8) NOT NULL,
+  `fk_livro` int(8) DEFAULT NULL,
+  `fk_cliente` int(8) DEFAULT NULL,
+  `fk_funcionario` int(8) DEFAULT NULL,
+  `pago` float(6,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -87,10 +123,24 @@ ALTER TABLE `autor`
   ADD PRIMARY KEY (`id_autor`);
 
 --
+-- Índices para tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`),
+  ADD UNIQUE KEY `cpf` (`cpf`);
+
+--
 -- Índices para tabela `editora`
 --
 ALTER TABLE `editora`
   ADD PRIMARY KEY (`id_editora`);
+
+--
+-- Índices para tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  ADD PRIMARY KEY (`id_funcionario`),
+  ADD UNIQUE KEY `cpf` (`cpf`);
 
 --
 -- Índices para tabela `generolivro`
@@ -108,6 +158,15 @@ ALTER TABLE `livro`
   ADD KEY `FK_genero` (`id_genero`);
 
 --
+-- Índices para tabela `venda`
+--
+ALTER TABLE `venda`
+  ADD PRIMARY KEY (`id_venda`),
+  ADD KEY `FK_livro` (`fk_livro`),
+  ADD KEY `FK_cliente` (`fk_cliente`),
+  ADD KEY `FK_funcionario` (`fk_funcionario`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
@@ -118,10 +177,22 @@ ALTER TABLE `autor`
   MODIFY `id_autor` int(8) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(8) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de tabela `editora`
 --
 ALTER TABLE `editora`
   MODIFY `id_editora` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  MODIFY `id_funcionario` int(8) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `generolivro`
@@ -136,6 +207,12 @@ ALTER TABLE `livro`
   MODIFY `id_livro` int(8) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT de tabela `venda`
+--
+ALTER TABLE `venda`
+  MODIFY `id_venda` int(8) NOT NULL AUTO_INCREMENT;
+
+--
 -- Restrições para despejos de tabelas
 --
 
@@ -146,6 +223,14 @@ ALTER TABLE `livro`
   ADD CONSTRAINT `FK_autor` FOREIGN KEY (`id_autor`) REFERENCES `autor` (`id_autor`),
   ADD CONSTRAINT `FK_editora` FOREIGN KEY (`id_editora`) REFERENCES `editora` (`id_editora`),
   ADD CONSTRAINT `FK_genero` FOREIGN KEY (`id_genero`) REFERENCES `generolivro` (`id_genero`);
+
+--
+-- Limitadores para a tabela `venda`
+--
+ALTER TABLE `venda`
+  ADD CONSTRAINT `FK_cliente` FOREIGN KEY (`fk_cliente`) REFERENCES `cliente` (`id_cliente`),
+  ADD CONSTRAINT `FK_funcionario` FOREIGN KEY (`fk_funcionario`) REFERENCES `funcionario` (`id_funcionario`),
+  ADD CONSTRAINT `FK_livro` FOREIGN KEY (`fk_livro`) REFERENCES `livro` (`id_livro`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
